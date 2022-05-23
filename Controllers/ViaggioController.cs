a5213ed31599a2123a352612bdc61605ac3a7dc1
Piangelica <101094023+Piangelica@users.noreply.github.com>
2022-05-23 17:56:32 +0200

    Add files via upload

---
+++ Controllers/ViaggioController.cs
@@ -0,0 +1,165 @@
+﻿ /*using Microsoft.AspNetCore.Mvc;
+using ViaggioAgencyWebApp.Controllers;
+using WebApp_TravelAgency.Models;
+
+namespace WebApp_TravelAgency.Controllers
+{
+    public class ViaggioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index
+        {
+            get
+            {
+
+                List<Viaggio> viaggi = new List<Viaggio>();
+
+                using (ViaggioContext database = new())
+                {
+                    viaggi = database.Viaggi.ToList<Viaggio>();
+                }
+
+                return View("HomePage", viaggi);
+            }
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            Viaggio viaggioFound = null;
+
+            using (ViaggioContext database = new ViaggioContext())
+            {
+                viaggioFound = database.Trips
+                    .Where(trip => trip.Id == id).FirstOrDefault();
+            }
+
+            if (viaggioFound != null)
+            {
+                return View("Details", viaggioFound);
+            }
+            else
+            {
+                return NotFound($"Il viaggio con l'Id {id} non è stato trovato");
+            }
+
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("FormTrip");
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Viaggio newViaggio)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("FormPost", newViaggio);
+            }
+
+            using (ViaggioContext database = new ViaggioContext())
+            {
+                Viaggio newViaggioToCreate = new Viaggio(newViaggio.Image, newViaggio.Title, newViaggio.Description, newViaggio.Length, newViaggio.Price);
+                database.Trips.Add(newViaggioToCreate);
+                database.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+
+            Viaggio viaggioToEdit = null;
+
+            using (ViaggioContext database = new ViaggioContext())
+            {
+                viaggioToEdit = database.Trips
+                    .Where(trip => trip.Id == id).FirstOrDefault();
+            }
+
+            if (viaggioToEdit == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View("Update", viaggioToEdit);
+            }
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Update(int id, Viaggio model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", model);
+            }
+
+            Viaggio viaggioOriginal = null;
+
+            using (ViaggioContext database = new ViaggioContext())
+            {
+                viaggioOriginal = database.Viaggi.Where(viaggio => viaggio.Id == id).FirstOrDefault();
+
+                if (viaggioOriginal != null)
+                {
+                    viaggioOriginal.Image = model.Image;
+                    viaggioOriginal.Title = model.Titolo;
+                    viaggioOriginal.Description = model.Description;
+                    viaggioOriginal.Length = model.Length;
+                    viaggioOriginal.Price = model.Price;
+
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+        }
+
+
+        [HttpPost]
+
+        public IActionResult Delete(int id)
+        {
+            using (ViaggioContext database = new ViaggioContext())
+            {
+                Viaggio viaggioToDelete = database.Viaggi.Where(viaggio => viaggio.Id == id).FirstOrDefault();
+
+                if (viaggioToDelete != null)
+                {
+                    database.Viaggi.Remove(viaggioToDelete);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+
+                }
+                else
+                {
+                    return NotFound();
+                }
+
+            }
+
+        }
+
+
+    }
+}*/
---
+++ Models/Viaggio.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp_TravelAgency.Models
+{
+    public class Viaggio
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Mi dispiace, l'URL dell'immagine è obbligatorio")]
+        [Url(ErrorMessage = "L'Url inserito non è valido")]
+        public string Immagine { get; set; }
+
+        [Required(ErrorMessage = "Mi dispiace, il campo titolo è obbligatorio")]
+        [StringLength(80, ErrorMessage = "Il titolo non può avere più di 80 caratteri")]
+        public string Titolo { get; set; }
+
+        [Required(ErrorMessage = "Mi dispiace,il campo descrizione è obbligatorio")]
+        [Column(TypeName = "text")]
+        public string Descrizione { get; set; }
+
+
+        public Viaggio(string immagine)
+        {
+            Immagine = immagine;
+        }
+
+        public Viaggio(string immagine, string titolo, string descrizione)
+        {
+            this.Immagine = immagine;
+            this.Titolo = titolo;
+            this.Descrizione = descrizione;
+          
+        }
+
+        public Viaggio(int id, string immagine, string titolo, string descrizione)
+        {
+            Id = id;
+            Immagine = immagine;
+            Titolo = titolo;
+            Descrizione = descrizione;
+        }
+    }
+}
+
