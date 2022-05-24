3b46fd4f09e427fd92a18e8bdb23807276a3cf53
Piangelica <101094023+Piangelica@users.noreply.github.com>
2022-05-24 14:59:23 +0200

    Add files via upload

--- Data/ViaggioContext.cs
+++ Data/ViaggioContext.cs
@@ -1,1 +1,17 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
+using WebApp_TravelAgency.Models;
+
+namespace WebApp_TravelAgency.Data
+{
+    public class ViaggioContext : DbContext
+    {
+        public DbSet<Viaggio> Viaggi { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=Database_Viaggio;Integrated Security=True");
+        }
+    }
+}
