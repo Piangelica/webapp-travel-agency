16c438465eb40993fb493e21e494fde1fb0caef4
Piangelica <101094023+Piangelica@users.noreply.github.com>
2022-05-24 15:18:10 +0200

    Add files via upload

---
+++ Controllers/API/ViaggiController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_TravelAgency.Data;
+using WebApp_TravelAgency.Models;
+
+namespace WebApp_TravelAgency.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ViaggiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string? search)
+        {
+            List<Viaggio> viaggi = new List<Viaggio>();
+
+            using (ViaggioContext db = new ViaggioContext())
+            {
+
+                if (search != null && search != "")
+                {
+                    viaggi = db.Viaggi.Where(viaggio => viaggio.Title.Contains(search) || viaggio.Descrizione.Contains(search)).ToList<Viaggio>();
+                }
+                else
+                {
+                    viaggi = db.Viaggi.ToList<Viaggio>();
+                }
+
+            }
+
+            return Ok(viaggi);
+        }
+
+
+        [HttpGet("{id}")]
+
+        public IActionResult Details(int id)
+        {
+            using (ViaggioContext db = new ViaggioContext())
+            {
+                try
+                {
+                    Viaggio vaggioTrovato = db.Viaggi
+                        .Where(trip => trip.Id == id)
+                        .FirstOrDefault();
+
+                    return Ok(vaggioTrovato);
+
+                }
+                catch (InvalidOperationException)
+                {
+                    return NotFound("Mi dispiace, il viaggio non è stato trovato");
+
+                }
+                catch (Exception)
+                {
+                    return BadRequest();
+                }
+            }
+        }
+
+
+
+    }
+}
